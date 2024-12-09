80fbd814fe28646796040a785e631a6f49030fee
codeforcoders-420 <javacodeforlife420@gmail.com>
2024-12-09 15:58:03 -0500

    Latest

--- src/main/java/com/test/spring/boot/readfile/RunstandardQuery.java
+++ src/main/java/com/test/spring/boot/readfile/RunstandardQuery.java
@@ -1,5 +1,8 @@
 package com.test.spring.boot.readfile;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RunstandardQuery {
 	
 	public static String runQuery (String inputField, String queryText, String selectedEnv) {
